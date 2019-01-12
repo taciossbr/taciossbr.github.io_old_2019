756094150f1a58faeb104c1a59330a9d70c342e5
Travis CI <travis@travis-ci.org>
2019-01-12 21:20:43 -0200

    travis10

--- push.sh
+++ push.sh
@@ -4,6 +4,8 @@ setup_git() {
   git config  user.email "travis@travis-ci.org"
   git config  user.name "Travis CI"
   git remote set-url origin https://${GITHUB_TOKEN}@github.com/taciossbr/taciossbr.github.io.git > /dev/null 2>&1
+  git pull origin master
+  
 }
 
 commit_website_files() {
